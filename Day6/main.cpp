9729d76916664c9b18d25675fc833c2c3287d67f
Matthew Baldwin <baldwinm@amazon.com>
2021-12-06 11:06:15 -0800

    Print part 1 and 2 properly

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <numeric>
 #include <vector>
+#include <chrono>
 
 constexpr int MaxLifetime = 8;
 using FishCounts = std::array<int64_t, MaxLifetime + 1>;
@@ -16,6 +17,7 @@ int main(int argc, char** argv) {
   
   FishCounts fish { };
   int64_t count = 0;
+
   {
     std::stringstream ss(input);
     std::string segment;
@@ -32,8 +34,11 @@ int main(int argc, char** argv) {
     count ++;
   }
 
-  int generation = 0;
-  while (generation < 256) {
+  const auto start = std::chrono::high_resolution_clock::now();
+  constexpr int pt_1_end = 80;
+  constexpr int pt_2_end = 256;
+
+  for (int generation = 0; generation < pt_1_end; generation++) {
     const int64_t recycled = fish[0];
     for (size_t i = 1; i < fish.size(); i ++) {
       fish[i - 1] = fish[i];
@@ -41,15 +46,29 @@ int main(int argc, char** argv) {
     fish[6] += recycled;
     count += recycled;
     fish[8] = recycled;
-
-    generation++;
-    DEBUG(std::cout << "Generation: " << generation << " count: " << count << std::endl);
-    if (generation == 80 || generation == 256) {
-      std::cout << "Generation: " << generation << " count: " << count << std::endl;
-    }
   }
+  std::cout << "Generation: " << pt_1_end << " count: " << count << std::endl;
 
+  for (int generation = pt_1_end; generation < pt_2_end; generation++) {
+    const int64_t recycled = fish[0];
+    for (size_t i = 1; i < fish.size(); i ++) {
+      fish[i - 1] = fish[i];
+    }
+    fish[6] += recycled;
+    count += recycled;
+    fish[8] = recycled;
+  }
+  std::cout << "Generation: " << pt_2_end << " count: " << count << std::endl;
 
+  const auto end = std::chrono::high_resolution_clock::now();
+
+  // Calculating total time taken by the program.
+  double time_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+  time_taken *= 1e-9;
+
+  std::cout << "Time taken by program is : " << std::fixed
+        << time_taken << std::setprecision(9);
+  std::cout << " sec" << std::endl;
   return 0;
 }
 
